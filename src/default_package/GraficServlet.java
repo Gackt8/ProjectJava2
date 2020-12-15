13503457316f2566a02c7a77d9c669c0ee824fea
Gackt8 <55741761+Gackt8@users.noreply.github.com>
2020-12-15 10:42:40 +0300

    Add files via upload

--- src/default_package/GraficServlet.java
+++ src/default_package/GraficServlet.java
@@ -2,8 +2,7 @@ package default_package;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,43 +21,23 @@ public class GraficServlet extends HttpServlet {
 
         try {
 
-
             int id = Integer.parseInt(request.getParameter("id"));
             PaymentValue product = BookDB.selectOne(id);
             request.setAttribute("product", product);
 
-//            int id = Integer.parseInt(request.getParameter("id"));
-
-//            PaymentValue productMy = BookDB.selectOne(id);
-//
-//            request.setAttribute("pro", productMy);
-
-//            ArrayList<Double> doubles = new ArrayList<>();
-//            for (int i = 1; i<productMy.getYear(); i++) {
-//
-//                doubles.add((double) i);
-//                doubles.add(productMy.getPaymentEveryMonth());
-//                doubles.add(productMy.getPercentEveryMonth());
-//                doubles.add(productMy.getPaymentEveryMonthWithPercent());
+//            for (int i = 0; i < product.getNum().size(); i++) {
+//                request.setAttribute("num", product.getNum().get(i));
 //
 //            }
-//
-//            request.setAttribute("pro", doubles);
+//            request.setAttribute("num", product.getNum());
 
 
-//            getServletContext().getRequestDispatcher("/grafic.jsp").forward(request, response);
-
-//            if(product!=null) {
-//                request.setAttribute("product", product);
-//                request.setAttribute("pr", product.getCreditPeriod());
-
+            PaymentValue productMy = BookDB.selectOne(id);
+            request.setAttribute("pro", productMy.getMonth());
 
 
                 getServletContext().getRequestDispatcher("/grafic.jsp").forward(request, response);
-//            }
-//            else {
-//                getServletContext().getRequestDispatcher("/notfound.jsp").forward(request, response);
-//            }
+
         }
         catch(Exception ex) {
             getServletContext().getRequestDispatcher("/notfound.jsp").forward(request, response);
